64e587e92d599bc7d5fd0a9df24fc5931f6d40bc
Shaojun Yang <xepher@outlook.com>
2026-07-29 18:08:11 +0800

    feat(benchmark): add multi-target framework support and ArrayCountBenchmark

---
+++ FirstBenchmark/ArrayCountBenchmark.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Jobs;
+
+namespace FirstBenchmark;
+
+// we should test mono4.9 and mono5.2
+[SimpleJob(RuntimeMoniker.Net80)]
+[SimpleJob(RuntimeMoniker.Net90)]
+[SimpleJob(RuntimeMoniker.Net10_0)]
+[MemoryDiagnoser]
+[EventPipeProfiler(EventPipeProfile.CpuSampling)]
+public class ArrayCountBenchmark
+{
+    private readonly IList<object> array = new string[0];
+
+    [Benchmark]
+    public int CountProblem() => array.Count;
+}
