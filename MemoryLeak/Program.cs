ef882503594001cd88c90f2515a2c3ad02b5a65d
Shaojun Yang <xepher@outlook.com>
2026-08-20 19:08:14 +0800

    feat: add MemoryLeak demo project

---
+++ MemoryLeak/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace MemoryLeak;
+
+class Program
+{
+    static List<string> times = new List<string>();
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Press any key to exit");
+        while (!Console.KeyAvailable)
+        {
+            times.Add(DateTime.Now.ToString());
+            Console.Write('.');
+            Thread.Sleep(10);
+        }
+    }
+}
